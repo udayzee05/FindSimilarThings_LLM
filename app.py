6801985cf0002ca3b07f310732db9d6e29b16b6d
Uday Zope <31612865+udayzee05@users.noreply.github.com>
2023-09-20 11:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,70 @@
+#Allows you to use Streamlit, a framework for building interactive web applications.
+#It provides functions for creating UIs, displaying data, and handling user inputs.
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import(
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-
-# App UI
-st.set_page_config(page_title="ChatModelApp ", page_icon = ":robot:")
-st.header("Hey , I am your Assistnat")
-
-if "sessionMessages" not in st.session_state:
-    st.session_state.sessionMessages=[
-        SystemMessage(content="You are helpful assistant.")
-        
-    ]
-
-def load_answer(question):
-    st.session_state.sessionMessages.append(HumanMessage(content=question))
-    assistant_answer = chat(st.session_state.sessionMessages)
-    st.session_state.sessionMessages.append(AIMessage(content=assistant_answer.content))
-    return assistant_answer.content
 
 
+#This module provides a way to interact with the operating system, such as accessing environment variables, working with files
+#and directories, executing shell commands, etc
+import os
+
+#Helps us generate embeddings
+#An embedding is a vector (list) of floating point numbers. The distance between two vectors measures their relatedness. 
+#Small distances suggest high relatedness and large distances suggest low relatedness.
+from langchain.embeddings import OpenAIEmbeddings
+
+
+#FAISS is an open-source library developed by Facebook AI Research for efficient similarity search and clustering of large-scale datasets, particularly with high-dimensional vectors. 
+#It provides optimized indexing structures and algorithms for tasks like nearest neighbor search and recommendation systems.
+from langchain.vectorstores import FAISS
+
+
+#load_dotenv() is a function that loads variables from a .env file into environment variables in a Python script. 
+#It allows you to store sensitive information or configuration settings separate from your code
+#and access them within your application.
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+#By using st.set_page_config(), you can customize the appearance of your Streamlit application's web page
+st.set_page_config(page_title="Educate Kids", page_icon=":robot:")
+st.header("Hey, Ask me something & I will give out similar things")
+
+#Initialize the OpenAIEmbeddings object
+embeddings = OpenAIEmbeddings()
+
+#The below snippet helps us to import CSV file data for our tasks
+from langchain.document_loaders.csv_loader import CSVLoader
+loader = CSVLoader(file_path='myData.csv', csv_args={
+    'delimiter': ',',
+    'quotechar': '"',
+    'fieldnames': ['Words']
+})
+
+#Assigning the data inside the csv to our variable here
+data = loader.load()
+
+#Display the data
+print(data)
+
+db = FAISS.from_documents(data, embeddings)
+
+#Function to receive input from user and store it in a variable
 def get_text():
-    input_text = st.text_input("you", key=input)
+    input_text = st.text_input("You: ", key= input)
     return input_text
 
-chat = ChatOpenAI(temperature=0)
 
-user_input = get_text()
+user_input=get_text()
+submit = st.button('Find similar Things')  
 
-submit = st.button('Generate')
 if submit:
-    response = load_answer(user_input)
-    st.subheader("Answer : ")
-    st.write(response,key=1)
-
     
+    #If the button is clicked, the below snippet will fetch us the similar text
+    docs = db.similarity_search(user_input)
+    print(docs)
+    st.subheader("Top Matches:")
+    st.text(docs[0])
+    st.text(docs[1].page_content)
+
